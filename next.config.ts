0372c941342b89361a5e9647b3c9bc1b650534f9
Brody Haar <brody.j.haar@vanderbilt.edu>
2026-07-06 15:15:42 -0700

    first deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/WetBulb",
+  assetPrefix: "/WetBulb/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
